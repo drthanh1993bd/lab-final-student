02b35bd71e367608d5570dbb8c7c2ff9e12decc4
drthanh1993bd <quangviet626@gmail.com>
2024-04-24 15:48:56 -0600

    commit

--- array_structures.c
+++ array_structures.c
@@ -7,27 +7,56 @@
 #define SIZE 19
 
 // define your struct here
+typedef struct 
+{
+	char * name;
+	int year;
+	int time;
+}PixarMovie;
 
 
 // function prototype to print the movie closest to avg time without going over goes here
-
+int calculateAverage(PixarMovie*movies, int count){
+	int total = 0;
+	for (int i = 0; i < count; i++)
+	{
+		total += movies[i].time;
+	}
+	return total / count;
+}
 
 int main() {
 	FILE * input;
 	char buffer[256];
 	input = fopen("pixar.txt", "r");
 	// declare your array of structures and any other needed variables here
-	
+	assert (input != NULL);
+	PixarMovie movies[SIZE];
+	int count = 0;
 
 	while (fgets(buffer, 256, input) != NULL) {
 		//fill in the array of structures here
+		movies[count].name = strdup(strtok(buffer, ","));
+        movies[count].year = atoi(strtok(NULL, ","));
+        movies[count].time = atoi(strtok(NULL, ","));
+        count++;
 		
 	}
 
     // call the print function here
-    	
+    	printClosestToAverage(movies, count);
+
+    for (int i = 0; i < count; i++) {
+        free(movies[i].name);
+    }
+
 
 	// free any malloced memory here
+	for (int i = 0; i < count; i++)
+	{
+		free(movies[i].name);
+	}
+	free (movies);
 	
 
 	fclose(input);
@@ -35,3 +64,20 @@ int main() {
 }
 
 //define the function to print the movie closest to avg time without going over here
+void printClosestToAverage(PixarMovie*movies, int count){
+	int average = calculateAverage(movies,count);
+	int closestIndex =0;
+	int closestDifference = abs(movies[0].time - average);
+	for (int i = 0; i < count; i++)
+	{
+		int difference = abs(movies[i].time - average);
+		if (difference < closestDifference)
+		{
+			closestIndex = i;
+			closestDifference = difference;
+		}
+		
+	}
+	printf("%s(%d):%d minutues\n", movies[closestIndex].name,movies[closestIndex].year,movies[closestIndex].time);
+	
+}
--- fizzbuzz.c
+++ fizzbuzz.c
@@ -2,6 +2,20 @@
 
 int main() {
 	//your code goes here.
-	
+
+	for(int i = 1; i < 100 ; i++){
+	if(i % 3 == 0 && i % 7 == 0){
+		printf("FizzBuzz\n");
+	}
+	else if(i % 3 == 0){
+		printf("Fizz\n");
+	} 
+	else if(i % 7 == 0){
+		printf("Fizz\n");
+	}
+	else{
+		printf("%d\n");
+	}
+	}
 	return 0;
 }
